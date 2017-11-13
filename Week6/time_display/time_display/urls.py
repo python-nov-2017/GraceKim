4f1d36e807666e5cb43c5abae6383ba348c3dc6d
Grace <gsongk@gmail.com>
2017-11-13 14:20:36 -0700

    fixed time

--- Week6/time_display/time_display/urls.py
+++ Week6/time_display/time_display/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^', include('apps.time_display.urls')),
+    url(r'^', include('apps.timedisplay.urls')),
 ]
