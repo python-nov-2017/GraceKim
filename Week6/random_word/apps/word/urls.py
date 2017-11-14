9786385f2acabf028b07432f7f767f3344431a56
Grace <gsongk@gmail.com>
2017-11-13 18:13:23 -0700

    Added random word

--- Week6/random_word/apps/word/urls.py
+++ Week6/random_word/apps/word/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^generate$', views.generate),
+    url(r'^reset$', views.reset),
 ]
--- Week6/random_word/apps/word/views.py
+++ Week6/random_word/apps/word/views.py
@@ -1,8 +1,26 @@
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+import random
+import string
 
-from django.shortcuts import render
+def random_word(n):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
 
-# Create your views here.
-def index(req):
-    return render(req, 'word/index.html')
+def index(request):
+    try:
+        request.session['tries']
+    except KeyError:
+        request.session['tries'] = 0
+    return render(request, "word/index.html")
+
+def generate(request):
+    request.session['tries'] += 1
+    request.session['word'] = random_word(10)
+    return redirect('/')
+
+def reset(request):
+    del request.session['tries']
+    del request.session['word']
+    return redirect('/')
