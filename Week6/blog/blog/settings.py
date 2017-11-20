d2ea2326709003aab30c6bc111b37b8ab949d3a3
Grace <gsongk@gmail.com>
2017-11-20 13:18:09 -0700

    fixed week 6

--- Week6/blog/blog/settings.py
+++ Week6/blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b=k@$0a)ostp2!el!%h*bzzasu4&de68+)+$)v7x!$2m@ul7nm'
+SECRET_KEY = '77&(7@g3+3vjsq14p29onikms7kgvusj^#)7-v5lzdrdf)*xz='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
